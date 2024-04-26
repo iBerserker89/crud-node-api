0d04fec20d6e417ef7d3c6bda36daa6b6ab49491
Luciano Barros <lucianof.barros89@gmail.com>
2024-04-25 23:59:54 -0300

    Add files via upload

---
+++ index.sample.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const Product = require('./models/product.model.js');
+const app = express();
+
+app.use(express.json());
+
+
+
+
+
+app.get('/', (req, res) => {
+    res.send('Hello from Node API Server');
+});
+
+
+app.post('/api/products', async (req, res) => {
+    try {
+        const product = await Product.create(req.body);
+        res.status(200).json(product);
+    } catch (error) {
+        res.status(500).json( { message: error.message} );
+    }
+});
+
+mongoose.connect("mongodb+srv://userid:<password>@database.fxrggkz.mongodb.net/?retryWrites=true&w=majority&appName=db")
+  .then(() => {
+    console.log('Connected to database!');
+    app.listen(3000, () => {
+        console.log('Server is running on port 3000');
+    });
+})
+.catch(() => console.log('Connection failed!'));
